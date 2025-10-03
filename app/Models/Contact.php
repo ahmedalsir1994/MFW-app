c9e764aa2de5006707214df83cab50a6149edf14
Ahmed Alsir <Ahmedalsirmubarak@hotmail.com>
2025-10-03 16:26:19 +0400

    Add Contact.php model with proper case-sensitive naming

---
+++ app/Models/Contact.php
@@ -0,0 +1,17 @@
+﻿<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Contact extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'job_title',
+        'company',
+        'country',
+    ];
+}
